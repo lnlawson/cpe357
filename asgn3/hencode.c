46adb9820624048eddf69cc1d556dc5f34d3ca10
Logan <lnlawson@calpoly.edu>
2018-05-03 17:11:10 -0700

    try

--- asgn3/hencode.c
+++ asgn3/hencode.c
@@ -35,8 +35,8 @@ int main(int argc, char **argv){
 		codeTable = encodeTable(treeList[0]);
 		writeHeader(outfile, infile, count);
 		writeBits(outfile, infile, codeTable);
-		freeTree(treeList[0]);
-		free(treeList);
+		// freeTree(treeList[0]);
+		// free(treeList);
 		freeTable(codeTable);
 	}
 
